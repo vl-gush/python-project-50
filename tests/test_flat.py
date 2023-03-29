4a5d1b27b8df785a5ee9516b110ab8fe7b5a2316
Влад Гуштын <vladislav3372@gmail.com>
2023-03-29 23:08:18 +0300

    Fix tests

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -2,12 +2,12 @@ from gendiff.engine import generate_diff
 
 
 def test_flat():
-    json_file_before = "tests/fixtures/flat_before.json"
-    json_file_after = "tests/fixtures/flat_after.json"
+    json_file1 = "tests/fixtures/flat1.json"
+    json_file2 = "tests/fixtures/flat2.json"
 
-    yaml_file_before = "tests/fixtures/flat_before.yaml"
-    yaml_file_after = "tests/fixtures/flat_after.yaml"
+    yaml_file1 = "tests/fixtures/flat1.yaml"
+    yaml_file2 = "tests/fixtures/flat2.yaml"
 
     result = open("tests/fixtures/flat_result.txt").read()
-    assert generate_diff(json_file_before, json_file_after) == result
-    assert generate_diff(yaml_file_before, yaml_file_after) == result
+    assert generate_diff(json_file1, json_file2) == result
+    assert generate_diff(yaml_file1, yaml_file2) == result
