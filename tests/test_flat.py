6cf1babb6264d63c517dbb79aa6d966e98197f60
Влад Гуштын <vladislav3372@gmail.com>
2023-03-29 22:47:25 +0300

    Update tests

--- tests/test_flat.py
+++ tests/test_flat.py
@@ -1,8 +1,13 @@
-from gendiff.gendiff import generate_diff
+from gendiff.engine import generate_diff
 
 
 def test_flat():
-    file_before = "tests/fixtures/json_flat_before.json"
-    file_after = "tests/fixtures/json_flat_after.json"
-    result = open("tests/fixtures/json_flat_result.txt").read()
-    assert generate_diff(file_before, file_after) == result
+    json_file_before = "tests/fixtures/json_flat_before.json"
+    json_file_after = "tests/fixtures/json_flat_after.json"
+
+    yaml_file_before = "tests/fixtures/yaml_flat_before.yaml"
+    yaml_file_after = "tests/fixtures/yaml_flat_after.yaml"
+
+    result = open("tests/fixtures/flat_result.txt").read()
+    assert generate_diff(json_file_before, json_file_after) == result
+    assert generate_diff(yaml_file_before, yaml_file_after) == result
