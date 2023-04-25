7c0fa8e5894841ecb985dd3dffaf1ecb1a01a4f9
Влад Гуштын <vladislav3372@gmail.com>
2023-04-26 00:22:45 +0300

    Update fixtures

--- tests/test_plain.py
+++ tests/test_plain.py
@@ -1,15 +1,27 @@
 from gendiff.engine import generate_diff
 from gendiff.parse import parse
-from gendiff.formatters.plain import formatter as plain
+from gendiff.formatters.plain import render as plain
 
 
-def test_plain():
+def test_flat():
+    json_file1 = parse("tests/fixtures/flat1.json")
+    json_file2 = parse("tests/fixtures/flat2.json")
+
+    yaml_file1 = parse("tests/fixtures/flat1.yaml")
+    yaml_file2 = parse("tests/fixtures/flat2.yaml")
+
+    result = open("tests/fixtures/plain_flat_result.txt").read()
+    assert generate_diff(json_file1, json_file2, plain) == result
+    assert generate_diff(yaml_file1, yaml_file2, plain) == result
+
+
+def test_nested():
     json_file1 = parse("tests/fixtures/nested1.json")
     json_file2 = parse("tests/fixtures/nested2.json")
 
     yaml_file1 = parse("tests/fixtures/nested1.yaml")
     yaml_file2 = parse("tests/fixtures/nested2.yaml")
 
-    result = open("tests/fixtures/plain_result.txt").read()
+    result = open("tests/fixtures/plain_nested_result.txt").read()
     assert generate_diff(json_file1, json_file2, plain) == result
     assert generate_diff(yaml_file1, yaml_file2, plain) == result
