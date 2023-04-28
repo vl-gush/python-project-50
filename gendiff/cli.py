12fceb0b8bb56f0c2fdad4a4b9743ea33aca8a84
Влад Гуштын <vladislav3372@gmail.com>
2023-04-28 23:10:28 +0300

    Update cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -12,14 +12,21 @@ FORMATS = {
 
 def generate_parser():
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.",
+        prog="gendiff",
+        usage="%(prog)s [options] <first_file> <second_file>",
+        description="Compares two configuration files \
+        and show difference."
     )
-    parser.add_argument(
-        "-f", "--format",
-        default="stylish",
-        help="set format of output (default: stylish)"
-    )
+    parser.add_argument("-f", "--format",
+                        choices=FORMATS.keys(),
+                        default="stylish",
+                        help='output format (default: "stylish")'
+                        )
+    parser.add_argument("-V", "--version",
+                        action="version",
+                        version="%(prog)s 0.6.0")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
+    return args
     return args
