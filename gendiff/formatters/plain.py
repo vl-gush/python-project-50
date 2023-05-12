92fd43ea1d8e2fe80c103112d64907f08650980f
Влад Гуштын <vladislav3372@gmail.com>
2023-05-12 14:30:55 +0300

    Update plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,31 +1,27 @@
-from gendiff.formatters.stylish import is_children, to_string
+from gendiff.formatters.stylish import to_string
 
 
 def render(data: dict, key_path: list = []) -> str:
-    keys = sorted(set(data))
+    keys = data.keys()
     lines = []
-    for key in keys:
-        if is_children(data[key]):
-            lines.append(render(data[key], key_path + [key]))
+    for status, key in keys:
+        if status == "changed":
+            if data[status, key][0] == "children":
+                value = data[status, key][1]
+                lines.append(render(value, key_path + [key]))
+            else:
+                value1, value2 = data[status, key][0][1], data[status, key][1][1]
+                key = ".".join(key_path + [key])
+                lines.append(f"Property '{key}' was updated. From {to_string(unpack_value(value1))} to {to_string(unpack_value(value2))}")
         else:
-            lines.append(generate_lines(key, data[key], key_path))
-    cleaning_from_None(lines)
+            value_type, value = data[status, key]
+            key = ".".join(key_path + [key])
+            if status == "added":
+                lines.append(f"Property '{key}' was added with value: {to_string(unpack_value(value))}")
+            elif status == "removed":
+                lines.append(f"Property '{key}' was removed")
+    cleaning_from_empty_lines(lines)
     return "\n".join(lines)
-
-
-def generate_lines(key, data, key_path):
-    status = data[0]
-    key_path = ".".join(key_path + [key])
-    if status == "added":
-        value = data[1]
-        return f"Property '{key_path}' was added with value: \
-{to_string(unpack_value(value))}"
-    elif status == "removed":
-        return f"Property '{key_path}' was removed"
-    elif status == "changed":
-        old_value, new_value = data[1:]
-        return f"Property '{key_path}' was updated. From \
-{to_string(unpack_value(old_value))} to {to_string(unpack_value(new_value))}"
 
 
 def unpack_value(value):
@@ -36,6 +32,6 @@ def unpack_value(value):
     return value
 
 
-def cleaning_from_None(data):
-    while None in data:
-        data.remove(None)
+def cleaning_from_empty_lines(data):
+    while "" in data:
+        data.remove("")
