d3b2bd2d88406da568d834c90b38cf7c482935a7
Влад Гуштын <vladislav3372@gmail.com>
2023-05-12 14:30:22 +0300

    Update engine.py

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -9,39 +9,46 @@ def data_comparison(data1, data2):
     keys = sorted(list(set(data1) | set(data2)))
     result = {}
     for key in keys:
-        result[key] = key_check(key, data1, data2)
+        status, value = key_check(key, data1, data2)
+        result[(status, key)] = value
     return result
 
 
-def key_check(key, file1: dict, file2: dict):
-    if key in file1 and key in file2:
-        return key_in_both_files(key, file1, file2)
-    elif key in file1:
-        return generate_values("removed", file1[key])
-    else:
-        return generate_values("added", file2[key])
+def key_check(key, data1, data2):
+    if key in data1 and key in data2:
+        if data1[key] == data2[key]:
+            status = "no changed"
+            value = generate_value(data1[key])
+        else:
+            status = "changed"
+            return status, key_changed(data1[key], data2[key])
+    elif key in data1:
+        status = "removed"
+        value = generate_value(data1[key])
+    elif key in data2:
+        status = "added"
+        value = generate_value(data2[key])
+    return status, value
 
 
-def key_in_both_files(key, file1, file2):
-    if file1[key] == file2[key]:
-        return generate_values("no changed", file1[key])
-    if is_children(file1[key]) and is_children(file2[key]):
-        return data_comparison(file1[key], file2[key])
-    elif is_children(file1[key]):
-        return [
-            "changed",
-            data_comparison(file1[key], file1[key]),
-            file2[key]
-        ]
-    else:
-        return ["changed", file1[key], file2[key]]
-
-
-def generate_values(status, value):
+def generate_value(value):
     if is_children(value):
-        return [status, data_comparison(value, value)]
-    return [status, value]
+        return ["children", data_comparison(value, value)]
+    return ["value", value]
+
+
+def key_changed(value1, value2):
+    if is_children(value1) and is_children(value2):
+        return ["children", data_comparison(value1, value2)]
+    return [generate_value(value1), generate_value(value2)]
 
 
 def is_children(value):
     return isinstance(value, dict)
+
+
+if __name__ == "__main__":
+    from gendiff.parse import parse
+    a = parse("tests/fixtures/nested1.json")
+    b = parse("tests/fixtures/nested2.json")
+    print(data_comparison(a, b))
