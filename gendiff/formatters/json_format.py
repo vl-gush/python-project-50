92248dd4a16dbbda993616e2b3c09f8536f74d6d
Влад Гуштын <vladislav3372@gmail.com>
2023-06-18 19:16:24 +0300

    Update json_format.py

--- gendiff/formatters/json_format.py
+++ gendiff/formatters/json_format.py
@@ -11,35 +11,30 @@ def generate_dict(data: dict) -> dict:
     result = {}
     for status, key in keys:
         if status == "changed":
-            result[key] = key_changed(status, data[status, key])
+            result[key] = key_changed(data[status, key])
         else:
             result[key] = generate_values(status, data[status, key])
     return result
 
 
-def key_changed(status: str, value: any) -> Union[dict, list]:
+def key_changed(value: any) -> Union[dict, list]:
     if value[0] == "children":
         value = value[1]
         return generate_dict(value)
     else:
-        value1_type, value1 = value[0]
-        value2_type, value2 = value[1]
-        if value1_type == "children":
-            value1 = generate_dict(value1)
-        if value2_type == "children":
-            value2 = generate_dict(value2)
-        return [status, value1, value2]
+        value1_type, old_value1 = value[0]
+        value2_type, old_value2 = value[1]
+        new_value1 = generate_dict(old_value1) \
+            if value1_type == "children" else old_value1
+        new_value2 = generate_dict(old_value2) \
+            if value2_type == "children" else old_value2
+        return ["changed", new_value1, new_value2]
 
 
 def generate_values(status: str, value: any) -> Union[dict, list]:
-    value_type, value = value
+    value_type = value[0]
+    new_value = generate_dict(value[1]) \
+        if value_type == "children" else value[1]
     if status == "no changed":
-        if value_type == "children":
-            return generate_dict(value)
-        else:
-            return value
-    else:
-        if value_type == "children":
-            return [status, generate_dict(value)]
-        else:
-            return [status, value]
+        return new_value
+    return [status, new_value]
