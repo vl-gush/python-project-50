2aa5db705b88c10a1912c8acc3350c75401a95bc
Влад Гуштын <vladislav3372@gmail.com>
2023-06-18 18:53:03 +0300

    Update json_format.py

--- gendiff/formatters/json_format.py
+++ gendiff/formatters/json_format.py
@@ -1,34 +1,45 @@
+from typing import Union
 import json
 
 
 def render(data: dict):
-    def inner(data):
-        keys = data.keys()
-        result = {}
-        for status, key in keys:
-            if status == "changed":
-                if data[status, key][0] == "children":
-                    value = data[status, key][1]
-                    result[key] = inner(value)
-                else:
-                    value1_type, value1 = data[status, key][0]
-                    value2_type, value2 = data[status, key][1]
-                    if value1_type == "children":
-                        value1 = inner(value1)
-                    if value2_type == "children":
-                        value2 = inner(value2)
-                    result[key] = [status, value1, value2]
-            else:
-                value_type, value = data[status, key]
-                if status == "no changed":
-                    if value_type == "children":
-                        result[key] = inner(value)
-                    else:
-                        result[key] = value
-                else:
-                    if value_type == "children":
-                        result[key] = [status, inner(value)]
-                    else:
-                        result[key] = [status, value]
-        return result
-    return json.dumps(inner(data), indent=4)
+    return json.dumps(generate_dict(data), indent=4)
+
+
+def generate_dict(data: dict) -> dict:
+    keys = data.keys()
+    result = {}
+    for status, key in keys:
+        if status == "changed":
+            result[key] = key_changed(status, data[status, key])
+        else:
+            result[key] = generate_values(status, data[status, key])
+    return result
+
+
+def key_changed(status: str, value: any) -> Union[dict, list]:
+    if value[0] == "children":
+        value = value[1]
+        return generate_dict(value)
+    else:
+        value1_type, value1 = value[0]
+        value2_type, value2 = value[1]
+        if value1_type == "children":
+            value1 = generate_dict(value1)
+        if value2_type == "children":
+            value2 = generate_dict(value2)
+        return [status, value1, value2]
+
+
+def generate_values(status: str, value: any) -> Union[dict, list]:
+    value_type, value = value
+    if status == "no changed":
+        if value_type == "children":
+            return generate_dict(value)
+        else:
+            return value
+    else:
+        if value_type == "children":
+            return [status, generate_dict(value)]
+        else:
+            return [status, value]
