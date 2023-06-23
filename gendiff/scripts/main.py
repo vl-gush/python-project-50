6585b47c2768281462de306e5e8e0b82447c0644
Влад Гуштын <vladislav3372@gmail.com>
2023-06-23 18:42:39 +0300

    Update script

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,12 +1,11 @@
 from gendiff.cli import generate_parser, FORMATS
 from gendiff.engine import generate_diff
-from gendiff.parse import parse
 
 
 def main():
     parser = generate_parser()
-    first_file = parse(parser.first_file)
-    second_file = parse(parser.second_file)
+    first_file = parser.first_file
+    second_file = parser.second_file
     formatter = FORMATS[parser.format]
     print(generate_diff(first_file, second_file, formatter))
 
