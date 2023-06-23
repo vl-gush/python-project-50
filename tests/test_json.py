e21f14c624c4ac04a3999e5c62f054f8046fe697
Влад Гуштын <vladislav3372@gmail.com>
2023-06-23 18:42:10 +0300

    Fix tests

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,14 +1,13 @@
 from gendiff.engine import generate_diff
-from gendiff.parse import parse
 from gendiff.formatters.json_format import render as json_format
 
 
 def test_flat():
-    json_file1 = parse("tests/fixtures/flat1.json")
-    json_file2 = parse("tests/fixtures/flat2.json")
+    json_file1 = "tests/fixtures/flat1.json"
+    json_file2 = "tests/fixtures/flat2.json"
 
-    yaml_file1 = parse("tests/fixtures/flat1.yaml")
-    yaml_file2 = parse("tests/fixtures/flat2.yaml")
+    yaml_file1 = "tests/fixtures/flat1.yaml"
+    yaml_file2 = "tests/fixtures/flat2.yaml"
 
     result = open("tests/fixtures/json_flat_result.txt").read()
     assert generate_diff(json_file1, json_file2, json_format) == result
@@ -16,11 +15,11 @@ def test_flat():
 
 
 def test_nested():
-    json_file1 = parse("tests/fixtures/nested1.json")
-    json_file2 = parse("tests/fixtures/nested2.json")
+    json_file1 = "tests/fixtures/nested1.json"
+    json_file2 = "tests/fixtures/nested2.json"
 
-    yaml_file1 = parse("tests/fixtures/nested1.yaml")
-    yaml_file2 = parse("tests/fixtures/nested2.yaml")
+    yaml_file1 = "tests/fixtures/nested1.yaml"
+    yaml_file2 = "tests/fixtures/nested2.yaml"
 
     result = open("tests/fixtures/json_nested_result.txt").read()
     assert generate_diff(json_file1, json_file2, json_format) == result
