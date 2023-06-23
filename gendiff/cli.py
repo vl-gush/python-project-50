817d5d8848cf1b60d169134bc0b5896f47004c6d
Влад Гуштын <vladislav3372@gmail.com>
2023-06-23 18:42:51 +0300

    ver 0.6.3

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -24,7 +24,7 @@ def generate_parser():
                         )
     parser.add_argument("-V", "--version",
                         action="version",
-                        version="%(prog)s 0.6.0")
+                        version="%(prog)s 0.6.3")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
